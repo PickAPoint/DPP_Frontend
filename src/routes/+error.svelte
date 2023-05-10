9e8dc3f4a185f4082512d9428ecb676c4d936368
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-05-10 18:01:31 +0100

    PIC-38 Add login page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,5 @@
-<script>
-    import { page } from '$app/stores';
-</script>
-
-<div class="flex flex-col items-center justify-center h-full">
-    <h1 class="text-4xl font-bold">{$page.status}</h1>
-    <h2 class="text-2xl font-semibold">{$page.url.pathname} {$page.error.message}</h2>
+<div class="w-full h-full flex flex-col justify-center items-center">
+    <h1 class="text-4xl font-bold">404</h1>
+    <h2 class="text-2xl font-semibold">Page not found</h2>
+    <a href="/" class="text-primary-700 hover:text-primary-700">Go back home</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<p>Home</p>
+<script>
+  import { Button } from 'flowbite-svelte'
+</script>
+
+
+<div class="w-full h-full relative">
+    <div class="md:w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 class="font-bold brand text-8xl mb-4">PickAPoint</h1>
+        <h2 class="text-xl mb-5">Say goodbye to missed deliveries and inconvenient trips to the post office. With our platform, stores become trusted partners, offering a convenient location for customers to collect their packages at their own convenience.</h2>
+        <Button color="primary" href="/register">Become a Pick-up Point</Button>
+    </div>
+</div>
