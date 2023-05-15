b62b81445ce90b189675cb9b1762f1889fc78a96
Diogo <d.magalhaes@ua.pt>
2023-05-15 01:48:14 +0100

    PIC-53 Admin dashboard Users

--- src/routes/manager/+page.svelte
+++ src/routes/manager/+page.svelte
@@ -1,1 +1,50 @@
-manager home
+<script>
+    import { Button, Table, TableBody, TableHead, TableBodyRow, TableBodyCell, TableHeadCell } from 'flowbite-svelte';
+    import UsersList from './UsersList.svelte';
+    // import UserStats from './UserStats.svelte';
+
+    // import UserStats from './UserStats.svelte';
+    
+    import "@carbon/styles/css/styles.css";
+    import "@carbon/charts/styles.css";
+
+
+    let admin = {
+        name: "Bill Gates",
+        email: "admin@ua.pt",
+    }
+
+    let users_list = [
+      { img:  "F",
+        name: "Fnac Aveiro", email: "fnac@aveiro.com", address:"Fórum Aveiro, R. do Batalhão de Caçadores 10, 3810-064 Aveiro"
+      },
+      { img:  "M",
+        name: "McDonald's Aveiro", email: "mcdonalds@aveiro.com", address:"R. das Pombas, 3810-150 Aveiro"
+      },
+    ];
+    
+</script>
+
+
+<div class="container my-12">
+
+
+    <div>
+        <p class="text-4xl font-bold inline-block">{admin.name}</p>
+        <span class="font-extralight italic mx-1 text-lg">Administrator</span>
+        
+        <!-- <div class="mt-4 mb-11">
+            <Button color="primary" on:click={() => true}>Manage Users</Button>
+        </div> -->
+    </div>
+
+    <div class="h-12">
+    </div>
+    
+    
+    <div>
+        <UsersList users_list={users_list}/>
+    </div>
+
+
+</div>
---
+++ src/routes/manager/UsersList.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { Card, Listgroup, Avatar } from "flowbite-svelte";
+
+    export let users_list = [];
+
+
+</script>
+
+
+<Card padding="xl" size="">
+<div class="flex justify-between items-center mb-4">
+    <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Pick Up Points</h5>
+    <a href="/manager/users" class="text-sm font-medium text-primary-900 hover:underline">
+        View all
+    </a>
+</div>
+<Listgroup items={users_list} let:item class="border-0 dark:!bg-transparent">
+    <div class="grid grid-cols-3 gap-x-[2%] grid-cols-[8%_max(30%)_1fr]">
+    <Avatar class="flex-shrink-0 truncate">{item.name[0]}</Avatar>
+    <div class="flex-1 min-w-0">
+        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+        {item.name}
+        </p>
+        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+        {item.email}
+        </p>
+    </div>
+    <div class="text-base font-semibold text-gray-900 truncate dark:text-white">
+        {item.address}
+    </div>
+    
+    </div>
+</Listgroup>
+</Card>
